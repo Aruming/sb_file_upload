d0b3f069af8f757ac3882474e918e877f4556873
Aruming <0430kar@naver.com>
2022-09-05 15:09:30 +0900

    세팅

---
+++ src/main/java/com/ll/exam/sb_file_upload/SbFileUploadApplication.java
@@ -0,0 +1,14 @@
+package com.ll.exam.sb_file_upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class SbFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFileUploadApplication.class, args);
+	}
+
+}
