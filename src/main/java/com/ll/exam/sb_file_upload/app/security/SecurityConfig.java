eea4e91117dd4cfa3327cc48d1e641edce876afd
Aruming <0430kar@naver.com>
2022-09-14 13:42:50 +0900

    로그인/로그아웃 상태에 따라 페이지 접근 권한 작업

--- src/main/java/com/ll/exam/sb_file_upload/app/security/SecurityConfig.java
+++ src/main/java/com/ll/exam/sb_file_upload/app/security/SecurityConfig.java
@@ -1,8 +1,9 @@
-package com.ll.exam.sb_file_upload.app.base;
+package com.ll.exam.sb_file_upload.app.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,6 +29,9 @@ public class SecurityConfig {
                         formLogin -> formLogin
                                 .loginPage("/member/login") // GET
                                 .loginProcessingUrl("/member/login") // POST
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/member/logout")
                 );
         return http.build();
     }
