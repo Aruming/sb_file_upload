29107e526a103c393bd68a2c49065b17358f7b4b
Aruming <0430kar@naver.com>
2022-09-15 15:59:20 +0900

    카카오톡 로그인

---
+++ src/main/java/com/ll/exam/sb_file_upload/app/member/exception/MemberNotFoundException.java
@@ -0,0 +1,4 @@
+package com.ll.exam.sb_file_upload.app.member.exception;
+
+public class MemberNotFoundException extends RuntimeException {
+}
---
+++ src/main/java/com/ll/exam/sb_file_upload/app/security/exception/OAuthTypeMatchNotFoundException.java
@@ -0,0 +1,4 @@
+package com.ll.exam.sb_file_upload.app.security.exception;
+
+public class OAuthTypeMatchNotFoundException extends RuntimeException {
+}
