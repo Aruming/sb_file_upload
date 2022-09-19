bf99c71e54039a9580cb3e7d9f171ebc17f2ccbf
Aruming <0430kar@naver.com>
2022-09-19 16:05:41 +0900

    개발환경에서 초기데이터로 게시물 1개(본문이미지 2개) 생성

--- src/main/java/com/ll/exam/sb_file_upload/app/article/service/ArticleService.java
+++ src/main/java/com/ll/exam/sb_file_upload/app/article/service/ArticleService.java
@@ -2,6 +2,7 @@ package com.ll.exam.sb_file_upload.app.article.service;
 
 import com.ll.exam.sb_file_upload.app.article.entity.Article;
 import com.ll.exam.sb_file_upload.app.article.repository.ArticleRepository;
+import com.ll.exam.sb_file_upload.app.fileUpload.service.GenFileService;
 import com.ll.exam.sb_file_upload.app.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
+    private final GenFileService genFileService;
 
     public Article write(Long authorId, String subject, String content) {
+        return write(new Member(authorId), subject, content);
+    }
+
+    public Article write(Member author, String subject, String content) {
         Article article = Article
                 .builder()
-                .author(new Member(authorId))
+                .author(author)
                 .subject(subject)
                 .content(content)
                 .build();
@@ -26,5 +32,9 @@ public class ArticleService {
 
     public Article getArticleById(Long id) {
         return articleRepository.findById(id).orElse(null);
+    }
+
+    public void addGenFileByUrl(Article article, String typeCode, String type2Code, int fileNo, String url) {
+        genFileService.addGenFileByUrl("article", article.getId(), typeCode, type2Code, fileNo, url);
     }
 }
