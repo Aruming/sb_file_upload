fb262e3455fe35f1bd860117811ed5dc5d4e13e5
Aruming <0430kar@naver.com>
2022-09-19 14:34:52 +0900

    글쓰기 처리

--- src/main/java/com/ll/exam/sb_file_upload/app/article/controller/input/ArticleForm.java
+++ src/main/java/com/ll/exam/sb_file_upload/app/article/controller/input/ArticleForm.java
@@ -1,6 +1,15 @@
 package com.ll.exam.sb_file_upload.app.article.controller.input;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
 public class ArticleForm {
+    @NotEmpty
     private String subject;
+    @NotEmpty
     private String content;
 }
---
+++ src/main/java/com/ll/exam/sb_file_upload/app/article/entity/Article.java
@@ -0,0 +1,27 @@
+package com.ll.exam.sb_file_upload.app.article.entity;
+
+import com.ll.exam.sb_file_upload.app.base.entity.BaseEntity;
+import com.ll.exam.sb_file_upload.app.member.entity.Member;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.io.File;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Article extends BaseEntity {
+    @ManyToOne
+    private Member author;
+    @Column(unique = true)
+    private String subject;
+    private String content;
+}
