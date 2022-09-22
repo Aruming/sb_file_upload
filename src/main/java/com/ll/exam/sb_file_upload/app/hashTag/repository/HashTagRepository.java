b7e77226fb338c586c7b4efcf7ae1cfa4e22e11f
Aruming <0430kar@naver.com>
2022-09-22 15:24:22 +0900

    해시태그 조회 테스트케이스 구현

--- src/main/java/com/ll/exam/sb_file_upload/app/hashTag/repository/HashTagRepository.java
+++ src/main/java/com/ll/exam/sb_file_upload/app/hashTag/repository/HashTagRepository.java
@@ -3,8 +3,11 @@ package com.ll.exam.sb_file_upload.app.hashTag.repository;
 import com.ll.exam.sb_file_upload.app.hashTag.entity.HashTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HashTagRepository extends JpaRepository<HashTag, Long> {
     Optional<HashTag> findByArticleIdAndKeywordId(Long articleId, Long keywordId);
+
+    List<HashTag> findAllByArticleId(Long articleId);
 }
--- src/test/java/com/ll/exam/sb_file_upload/MemberTests.java
+++ src/test/java/com/ll/exam/sb_file_upload/MemberTests.java
@@ -1,6 +1,5 @@
 package com.ll.exam.sb_file_upload;
 
-import com.ll.exam.sb_file_upload.app.home.controller.HomeController;
 import com.ll.exam.sb_file_upload.app.member.controller.MemberController;
 import com.ll.exam.sb_file_upload.app.member.entity.Member;
 import com.ll.exam.sb_file_upload.app.member.service.MemberService;
@@ -13,53 +12,33 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.client.RestTemplate;
 
 import javax.transaction.Transactional;
-
 import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
 @ActiveProfiles("test")
-public class AppTest {
+class MemberTests {
 
     @Autowired
     private MockMvc mvc;
 
     @Autowired
     private MemberService memberService;
-
-    @Test
-    @DisplayName("메인화면에서는 안녕이 나와야 한다.")
-    void t1() throws Exception {
-        // WHEN
-        // GET /
-        ResultActions resultActions = mvc
-                .perform(get("/"))
-                .andDo(print());
-
-        // THEN
-        // 안녕
-        resultActions
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(handler().handlerType(HomeController.class))
-                .andExpect(handler().methodName("showMain"))
-                .andExpect(content().string(containsString("안녕")));
-    }
 
     @Test
     @DisplayName("회원의 수")
@@ -72,8 +51,6 @@ public class AppTest {
     @DisplayName("user1로 로그인 후 프로필페이지에 접속하면 user1의 이메일이 보여야 한다.")
     @WithUserDetails("user1")
     void t3() throws Exception {
-        // mockMvc로 로그인 처리
-
         // WHEN
         // GET /
         ResultActions resultActions = mvc
@@ -83,6 +60,7 @@ public class AppTest {
                 .andDo(print());
 
         // THEN
+        // 안녕
         resultActions
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(handler().handlerType(MemberController.class))
@@ -103,6 +81,7 @@ public class AppTest {
                 .andDo(print());
 
         // THEN
+        // 안녕
         resultActions
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(handler().handlerType(MemberController.class))
