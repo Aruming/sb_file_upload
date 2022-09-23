38d460e03a6a99d0e3f87b76d1dad349b81e8139
Aruming <0430kar@naver.com>
2022-09-23 09:55:09 +0900

    게시물 리스트

--- src/main/java/com/ll/exam/sb_file_upload/app/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/exam/sb_file_upload/app/article/repository/ArticleRepository.java
@@ -3,5 +3,8 @@ package com.ll.exam.sb_file_upload.app.article.repository;
 import com.ll.exam.sb_file_upload.app.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findAll();
 }
